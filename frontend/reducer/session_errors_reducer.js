061a959d4fc9ecf15f304621b62962c647750596
GeniusSniper <tiwei61342@gmail.com>
2021-07-07 15:22:36 -0500

    clean code up

--- frontend/reducer/session_errors_reducer.js
+++ frontend/reducer/session_errors_reducer.js
@@ -1,12 +1,10 @@
-import {RECEIVE_SESSION_ERRORS, RECEIVE_CURRENT_USER, REMOVE_ERRORS} from '../actions/action';
+import {RECEIVE_SESSION_ERRORS, REMOVE_ERRORS} from '../actions/action';
   
   export default (state = [], action) => {
     Object.freeze(state);
     switch (action.type) {
       case RECEIVE_SESSION_ERRORS:
         return action.errors;
-      case RECEIVE_CURRENT_USER:
-        return [];
       case REMOVE_ERRORS:
         return [];
       default:
