7a70d70b149fa8ecbfa1b674aebcaaab58e983ca
GeniusSniper <tiwei61342@gmail.com>
2021-07-14 09:37:18 -0500

    improve item show page picture css

--- frontend/components/items/item.jsx
+++ frontend/components/items/item.jsx
@@ -17,27 +17,34 @@ class Item extends React.Component{
         const item = this.props.item;
         if(!item) return null;
         const itemimgs = item.image_url.map((img, j) => (
-            <img src={img} alt="broke" key={img} className='itemimg' onMouseOver={() => this.setState({i: j})}/>
+            <div className='itemImgsGrid'  key={img}>
+                <div className='itemImgBlock'>
+                    <img src={img} alt="broke" className='itemimg' onMouseOver={() => this.setState({i: j})}/>
+                </div>
+            </div>
         ))
         return(
             <div className='eachItem'>
-                <div className='leftSession'>
-                    <div className='itemimgs'>
-                        {itemimgs}
+                <div className='eachItemFlex'>
+                    <div className='leftSession'>
+                        <div className='itemimgs'>
+                            {itemimgs}
+                        </div>
+                        <div className='eachItemMainImg'>
+                            <img src={`${item.image_url[this.state.i]}`} alt="imgs" className='eachImgPit'/>
+                        </div>
                     </div>
-                    <img src={`${item.image_url[this.state.i]}`} alt="imgs" className='eachImgPit'/>
-                    <br />
-                </div>
-                <div className='midSession'>
-                    <h1 className='eachItemTitle'>{item.item_name}</h1>
-                    <br />
-                    <p className='itemDescrption'>{item.descrption}</p>
-                </div>
-                <div className='rightSession'>
-                    <p>${item.item_price}</p>
-                    <p>numbers of item left: {item.num_of_item_left}</p>
-                    <button className='addCartButton'>Add to Cart</button>
-                    <button className='purchaseButton'>Buy Now</button>
+                    <div className='midSession'>
+                        <h1 className='eachItemTitle'>{item.item_name}</h1>
+                        <br />
+                        <p className='itemDescrption'>{item.descrption}</p>
+                    </div>
+                    <div className='rightSession'>
+                        <p>${item.item_price}</p>
+                        <p>numbers of item left: {item.num_of_item_left}</p>
+                        <button className='addCartButton'>Add to Cart</button>
+                        <button className='purchaseButton'>Buy Now</button>
+                    </div>
                 </div>
             </div>
         )
