11f2ecffb75bff47a16930854466afec343abb28
GeniusSniper <tiwei61342@gmail.com>
2021-08-12 12:34:19 -0500

    adding css on search page

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -9,18 +9,30 @@ class Search extends React.Component {
         let items = this.props.items;
         if(items.length === 0) return null;
         let need = this.props.match.params.keyword;
-        let result = items.map(item => {
+        let result = [];
+        items.forEach(item => {
             if(item.item_name.includes(need) || item.descrption.includes(need)){
-                return (
+                result.push(
                     <div key={item.item_name} className='searchItems'>
-                        <div><img src={item.image_url[0]} alt="img" /></div>
+                        <div><img src={item.image_url[0]} alt="img" className='searchImgs'/></div>
                         <div>{item.item_name}</div>
-                        <div>{item.item_price}</div>
+                        <div>${item.item_price}</div>
                     </div>
                 )
             }
-            return null;
         });
+        if(result.length === 0){
+            result = (
+                <div>
+                    <h2>
+                        No results for {need}.
+                    </h2>
+                    <h2>
+                        Try checking your spelling or use more general terms
+                    </h2>
+                </div>
+            )
+        }
         return (
             <div className='searchResult'>
                 {result}
