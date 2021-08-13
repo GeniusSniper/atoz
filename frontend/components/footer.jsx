2c163e29b003da0521447b3fa9bc1d70fb8b8003
GeniusSniper <tiwei61342@gmail.com>
2021-08-13 13:28:24 -0500

    adding icons to the footer

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -5,10 +5,18 @@ export default () => {
         <div className='footer'>
             <div className='footerAbout'>About the developer: Ti Wei</div>
             <div className='footerBox'>
-                <div>Linkin</div>
-                <div>Github</div>
-                <div>angel list</div>
-                <div>source code</div>
+                <div>
+                    <img src="https://atoz-seeds.s3.us-east-2.amazonaws.com/LI-In-Bug.png" alt="LinkIn" className='icon' />
+                </div>
+                <div>
+                    <img src="https://atoz-seeds.s3.us-east-2.amazonaws.com/GitHub-Mark-32px.png" alt="github" />
+                </div>
+                <div>
+                    <img src="https://atoz-seeds.s3.us-east-2.amazonaws.com/AngelList_Black_Victory_Hand.png" alt="AngelList" className='icon' />
+                </div>
+                <div>
+                    <img src="https://atoz-seeds.s3.us-east-2.amazonaws.com/atoz_logo_black.png" alt="SourceCode" className='icon'/>
+                </div>
             </div>
         </div>
     )
