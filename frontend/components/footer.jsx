3da5f71427b67665ca5e95c6dfa87e5992514691
GeniusSniper <tiwei61342@gmail.com>
2021-08-13 13:01:29 -0500

    adding footer

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 export default () => {
     return (
-        <div>
-            <div>Ti</div>
+        <div className='footer'>
+            <div className='footerAbout'>About the developer: Ti Wei</div>
+            <div className='footerBox'>
+                <div>Linkin</div>
+                <div>Github</div>
+                <div>angel list</div>
+                <div>source code</div>
+            </div>
         </div>
     )
 }
