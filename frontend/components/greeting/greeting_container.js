5639784e43154134573112a2827c1d901e506719
GeniusSniper <tiwei61342@gmail.com>
2021-09-13 12:56:53 -0500

    styling details

--- frontend/components/greeting/greeting_container.js
+++ frontend/components/greeting/greeting_container.js
@@ -2,9 +2,11 @@ import { connect } from 'react-redux';
 import { logout } from '../../actions/session_action';
 import Greeting from './greeting';
 
-const mst = ({ session, entities: { users } }) => ({
+const mst = ({ session, entities: { users } }) => {
+  
+  return ({
     currentUser: users[session.id]
-});
+})};
 
 const mdt = dispatch => ({
   logout: () => dispatch(logout())
