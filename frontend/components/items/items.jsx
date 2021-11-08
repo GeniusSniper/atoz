fbf33f5f91277d13918b95915875b0573b6b6a82
GeniusSniper <tiwei61342@gmail.com>
2021-11-08 11:20:23 -0600

    adding notes on thing I could update on

--- frontend/components/items/items.jsx
+++ frontend/components/items/items.jsx
@@ -18,6 +18,7 @@ class Items extends React.Component{
                                 <div className='gridImgBox'>
                                     <Link to={`items/${item.id}`} className='gridItemLink'><img src={`${item.image_url[0]}`} alt="img_of_each_item" className='gridItemImg'/></Link>
                                 </div>
+                                <div>${priceToS(item.item_price)}</div>
                             </div>
                         </div>
                     ))}
@@ -41,6 +42,21 @@ class Items extends React.Component{
 
 export default Items;
 
+let priceToS = num => {
+    if(num.length < 4) return num;
+    let str = '';
+    let arr = num.split('.');
+    for(let i = 0; i < arr[0].length; i++){
+        if((arr[0].length - i) % 3 === 0 && i !==0){
+            str += ',' + arr[0][i]
+        } else {
+            str += arr[0][i];
+        }
+    }
+    str += '.' + arr[1].substring(0,2);
+    return str;
+}
+
 let itemName = name => {
-    return name.length > 17 ? name.slice(0, 17) + '...' : name;
+    return name.length > 30 ? name.slice(0, 30) + '...' : name;
 }
